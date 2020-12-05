af6e6fd09921cbf21ed3fda7584216a75771f64c
Daniela Carrillo <74390016+daniela0723@users.noreply.github.com>
2020-12-05 14:10:24 -0800

    Add files via upload

--- loops.js
+++ loops.js
@@ -1,30 +1,28 @@
 let a = 0;
 while (a <= 100) {
-    console.log(a);
-    a += 2;
+  console.log(a);
+  a += 2;
 }
 
 let b = 100;
 while (b >= 0) {
-    console.log(b);
-    b -= 3;
+  console.log(b);
+  b -= 3;
 }
 
-
-for (let i = 1; i <= 100 ; i++) {
-    if (i % 2 !== 0) {
-        console.log(i);
-    } 
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
 }
 
-for (let a = 0; a <= 100; a++) {
-    if (a % 3 == 0) {
-        console.log('Hello');
-     }
-     if (a % 5 == 0) {
-        console.log('World');
-    }
-    if (a % 3 == 0 && a % 5 == 0) {
-        console.log('HelloWorld');
-    }
+for (let i = 0; i <= 100; i++) {
+  let result = "";
+  if (i % 3 == 0) {
+    result += "Hello";
+  }
+  if (i % 5 == 0) {
+    result += "World";
+  }
+  console.log(result || i); // If conditions are not met, then the number is displayed.
 }
